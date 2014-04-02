0ca99163da97cbb5d32014ebcd5ff3c6c94cc4fd
Ivo Georgiev <ivo@linvo.org>
2014-04-02 15:35:01 +0300

    LinvoFS - first commit

---
+++ linvofs.js
@@ -0,0 +1,19 @@
+/* TODO: split that file into parts
+ */
+
+/* Backends
+ */
+var torrentEngine = require("torrent-stream");
+
+function openPath(path, cb)
+{
+}
+
+
+/* Front-end: HTTP
+ */
+ 
+ 
+/* Front-end: FUSE
+ */
+// TODO
