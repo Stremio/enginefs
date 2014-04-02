c40702b6e7355dce3df4f7dfd56dcd51e5f204a8
Ivo Georgiev <ivo@linvo.org>
2014-04-02 16:32:48 +0300

    Accessing files from torrents now works

--- linvofs.js
+++ linvofs.js
@@ -9,7 +9,7 @@ var rangeParser = require("range-parser");
 var mime = require("mime");
 var pump = require("pump");
 
-/* Backends
+/* Backend
  */
 var engine = require("torrent-stream");
 engine.engines = { };
@@ -41,21 +41,17 @@ function openPath(path, cb)
         createEngine(infoHash, function(err, engine)
         {
             if (err) return cb(err);
-        
-            // TODO: check if file index is valid once engine ready
-            console.log(engine.files);
-            console.log(infoHash);
-            console.log(i);
+            if (! engine.files[i]) return cb(new Error("Torrent does not contain file with index "+i));
+            
+            cb(null, engine.files[i]);
         });
-                
         return;
     }
     
     // length: 64 ; Linvo Hash ; TODO
     if (parts[0] && parts[0].length == 64)
     {
-        /*
-         * Large TODO
+        /* Large TODO
          */
         return cb(new Error("Not implemented yet"));
     }
@@ -63,69 +59,38 @@ function openPath(path, cb)
     cb(new Error("Cannot parse path"));
 }
 
-
 /* Front-end: HTTP
  */
 function createServer()
 {
 	var server = http.createServer();
 
-    /*
-	var onready = function() {
-		if (typeof index !== 'number') {
-			index = e.files.reduce(function(a, b) {
-				return a.length > b.length ? a : b;
-			});
-			index = e.files.indexOf(index);
-		}
-
-		e.files[index].select();
-		server.index = e.files[index];
-	};
-
-	if (e.torrent) onready();
-	else e.on('ready', onready);
-*/
-
 	server.on("request", function(request, response) {
 		var u = url.parse(request.url);
 
-		if (u.pathname === '/favicon.ico') return response.end();
-		if (u.pathname === '/') u.pathname = '/'+index;
+		if (u.pathname === "/favicon.ico") return response.end();
         
         openPath(u.pathname, function(err, handle)
         {
+            var range = request.headers.range;
+            range = range && rangeParser(handle.length, range)[0];
+            response.setHeader("Accept-Ranges", "bytes");
+            response.setHeader("Content-Type", mime.lookup(handle.name));
+
+            if (!range) {
+                response.setHeader("Content-Length", handle.length);
+                if (request.method === "HEAD") return response.end();
+                pump(handle.createReadStream(), response);
+                return;
+            }
+
+            response.statusCode = 206;
+            response.setHeader("Content-Length", range.end - range.start + 1);
+            response.setHeader("Content-Range", "bytes "+range.start+"-"+range.end+"/"+handle.length);
+
+            if (request.method === "HEAD") return response.end();
+            pump(handle.createReadStream(range), response);           
         });
-        
-        /*
-		var i = Number(u.pathname.slice(1));
-
-		if (isNaN(i) || i >= e.files.length) {
-			response.statusCode = 404;
-			response.end();
-			return;
-		}
-
-		var file = e.files[i];
-		var range = request.headers.range;
-		range = range && rangeParser(file.length, range)[0];
-		response.setHeader('Accept-Ranges', 'bytes');
-		response.setHeader('Content-Type', mime.lookup(file.name));
-
-		if (!range) {
-			response.setHeader('Content-Length', file.length);
-			if (request.method === 'HEAD') return response.end();
-			pump(file.createReadStream(), response);
-			return;
-		}
-
-		response.statusCode = 206;
-		response.setHeader('Content-Length', range.end - range.start + 1);
-		response.setHeader('Content-Range', 'bytes '+range.start+'-'+range.end+'/'+file.length);
-
-		if (request.method === 'HEAD') return response.end();
-		pump(file.createReadStream(range), response);
-        */
     });
 
 	return server;    
@@ -134,5 +99,10 @@ function createServer()
 /* Front-end: FUSE
  */
 // TODO
+
+module.exports = {
+    http: createServer,
+    // fuse: TODO
+};
 
 createServer().listen(7777);
