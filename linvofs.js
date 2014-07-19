bc414cdc4c6e8a8d14b37fe3b63b28edd184c693
Ivo Georgiev <ivo@linvo.org>
2014-07-19 20:52:06 +0300

    revert that last condition

--- linvofs.js
+++ linvofs.js
@@ -255,7 +255,7 @@ LinvoFS.on("opened", function(infoHash, fileIndex, e)
         var files = engines[hash].files;
 
         if (policy.STOP_BKG_DOWNLOAD) files.forEach(function(f) { if (!f.__linvofs_active) f.deselect() }); // Deselect files
-        if (policy.STOP_SWARMS && !isActive(engines[hash]) && Date.now()-e.__linvofs_last_active.getTime() > 60*1000) engines[hash].swarm.pause(); // Stop swarms
+        if (policy.STOP_SWARMS && !isActive(engines[hash])/* && Date.now()-e.__linvofs_last_active.getTime() > 60*1000*/) engines[hash].swarm.pause(); // Stop swarms
     }
 });
 
