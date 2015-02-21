5d74b1266331f7c7e36e2da0e98c261c9b990ddb
Ivo Georgiev <ivo@linvo.org>
2015-02-22 01:00:08 +0200

    More notices for headers which should be copied from peerflix

--- enginefs.js
+++ enginefs.js
@@ -129,6 +129,9 @@ function createServer(port)
             response.setHeader("Cache-Control", "max-age=0, no-cache");
             
             // CORS? research in peerflix - https://github.com/mafintosh/peerflix/blob/master/index.js
+            // https://github.com/mafintosh/peerflix/commit/1ff1540d8b200b43064db51a043f885f79e14868 
+            // CORS for chromecast - https://github.com/mafintosh/peerflix/commit/9f22fb17ec7bc747f7b7dfa0e80951a638713220
+            // CORS byte ranges - https://github.com/mafintosh/peerflix/commit/4bf42ee93eabf679410797c54f65f49d36cf3410
             //response.setHeader("Access-Control-Max-Age", "1728000");
 
             if (!range) {
